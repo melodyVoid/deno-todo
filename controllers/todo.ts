fb4ac47cc1ae95f2a142e863705be9932754bcfe
melodyVoid <botaoxyz@gmail.com>
2020-05-30 20:35:10 +0800

    feat: 测试所有接口跑通

--- controllers/todo.ts
+++ controllers/todo.ts
@@ -1,4 +1,4 @@
-import { Response } from '../deps.ts'
+import { Request, Response } from '../deps.ts'
 import { _getTodos } from '../services/todo.ts'
 
 /**
@@ -13,4 +13,41 @@ export const getTodos = async ({ response }: { response: Response }) => {
     message: '获取成功',
     data: todoList,
   }
+}
+
+
+/**
+ * @description 新增 Todo
+ * @route POST /todos
+ * @date 2020/05/30
+ */
+export const addTodo = async ({ request, response}: { request: Request, response: Response}) => {
+  response.body = 'post'
+}
+
+/**
+ * @description 获取 Todo 详情
+ * @route GET /todos/:id
+ * @date 2020/05/30
+ */
+export const getTodoInfo = async ({ request, response}: { request: Request, response: Response}) => {
+  response.body = 'getInfo'
+}
+
+/**
+ * @description 修改 Todo
+ * @route PUT /todos/:id
+ * @date 2020/05/30
+ */
+export const updateTodo = async ({ request, response}: { request: Request, response: Response}) => {
+  response.body = 'put'
+}
+
+/**
+ * @description 删除 Todo
+ * @route DELETE /todos/:id
+ * @date 2020/05/30
+ */
+export const deleteTodo = async ({ request, response}: { request: Request, response: Response}) => {
+  response.body = 'delete'
 }
--- routes/index.ts
+++ routes/index.ts
@@ -1,8 +1,19 @@
 import { Router } from '../deps.ts'
-import { getTodos } from '../controllers/todo.ts'
+import {
+  getTodos,
+  getTodoInfo,
+  addTodo,
+  updateTodo,
+  deleteTodo,
+} from '../controllers/todo.ts'
 
 const router = new Router()
 
-router.get('/todos', getTodos)
+router
+  .get('/todos', getTodos)
+  .get('/todos/:id', getTodoInfo)
+  .post('/todos', addTodo)
+  .put('/todos/:id', updateTodo)
+  .delete('/todos/:id', deleteTodo)
 
 export default router
